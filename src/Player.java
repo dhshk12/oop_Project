b88d613719d55d9d6e64a980c2e1c4fa87afb09a
Donghyun <dhshk12@gmail.com>
2018-11-15 14:56:49 +0800

    added some classes

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    private String name;
+
+    int[][] board= new int[9][7];
+
+
+
+
+    public Player() {}
+    public Player(String name)
+    {
+        this.name=name;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args)
+    {
+        String name1;
+        String name2;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter player X's name:");
+        in.hasNext();
+
+    }
+}
