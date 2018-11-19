81cdbb3339c59c77dc715da824fc580937751a37
Donghyun <dhshk12@gmail.com>
2018-11-19 20:07:28 +0800

    testing

--- src/main.java
+++ src/main.java
@@ -3,13 +3,36 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args)
     {
+
         String name1;
         String name2;
+        Player p1;
+        Player p2;
+        int opt;
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter player X's name:");
-        in.hasNext();
+        System.out.println("Press (1):New Game, (2):Saved Game:");
+        opt=in.nextInt();
+        if(opt==1)
+        {
+            p1=new Player();
+            p2=new Player();
+            Game g1= new Game(p1,p2);
+            g1.start();
+
+        }else if(opt==2)
+        {
+
+        }else
+        {
+
+        }
+
+
+
+
+
 
     }
 }
